dc8f853981d0e5f16391bb83648403fae270a3c1
Jo Lily <lily@example.com>
2026-01-25 00:17:24 +0900

    REPLの仮実装を追加

--- main.go
+++ main.go
@@ -1,1 +1,19 @@
-package monkeinterpreter
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/user"
+
+	"github.com/Calvados-1997/monkeyInterpreter/repl"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s, Welcome to Monkey language World!\n", user.Username)
+	fmt.Printf("Type command you want to execute\n")
+	repl.Start(os.Stdin, os.Stdout)
+}

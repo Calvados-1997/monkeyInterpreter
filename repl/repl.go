323853faa29e12b949390eab200b35cdfad2336c
Jo Lily <lily@example.com>
2026-01-26 22:09:36 +0900

    コメント追加

--- repl/repl.go
+++ repl/repl.go
@@ -24,6 +24,7 @@ func Start(in io.Reader, out io.Writer) {
 		line := scanner.Text()
 		l := lexer.New(line)
 
+		// このfor文はfor i := 0; i < len(string); i++と同じ構文
 		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
 			fmt.Printf("%+v\n", tok)
 		}

a0d53a9bb6a2b961c335031c48c919df21ae773b
Jo Lily <lily@example.com>
2026-01-29 23:03:41 +0900

    astの実装を追加

---
+++ ast/ast.go
@@ -0,0 +1,48 @@
+package ast
+
+import "github.com/Calvados-1997/monkeyInterpreter/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+// 文
+type Statement interface {
+	Node
+	statementNode()
+}
+
+// 式
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+type LetStatement struct {
+	Token token.Token // token.Letトークン
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.TokenLiteral() }
+
+type Identifier struct {
+	Token token.Token // token.IDENT トークン
+	Value string
+}
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
